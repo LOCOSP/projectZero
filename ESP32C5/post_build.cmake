72776fe2d99baad1f851f8321219dd920dd78ae4
Nigdzie <46287652+Nigdzie@users.noreply.github.com>
2025-10-16 21:40:24 +0200

    Fix Lang in post_build.

--- ESP32C5/post_build.cmake
+++ ESP32C5/post_build.cmake
@@ -8,23 +8,19 @@ if(NOT DEFINED APP)
   message(FATAL_ERROR "post_build.cmake: APP is not set")
 endif()
 
-# Upewnij się, że katalog docelowy istnieje
 file(MAKE_DIRECTORY "${DEST}")
 
-# Usuń TYLKO stare pliki .bin (np. zostaw pythonowe .py w spokoju)
 file(GLOB OLD_BINS "${DEST}/*.bin")
 if(OLD_BINS)
   file(REMOVE ${OLD_BINS})
 endif()
 
-# Zdefiniuj źródła do skopiowania
 set(SRCS
-  "${BLD}/${APP}"                            # np. build/projectZero.bin
-  "${BLD}/bootloader/bootloader.bin"         # bootloader
-  "${BLD}/partition_table/partition-table.bin" # tabela partycji (standardowa ścieżka w IDF)
+  "${BLD}/${APP}"
+  "${BLD}/bootloader/bootloader.bin"
+  "${BLD}/partition_table/partition-table.bin"
 )
 
-# Skopiuj istniejące pliki
 foreach(F ${SRCS})
   if(EXISTS "${F}")
     file(COPY "${F}" DESTINATION "${DEST}")
